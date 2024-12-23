1133d8f73afb5474fe9f77cfc1e179bda61fde2d
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-12-23 15:24:51 +0500

    JWT Manager paydalanıwǵa tayar

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_babel import Babel
+from flask_jwt_extended import JWTManager
 from src.routes import register_routes
 from src.infrastructure.models import init_db
 from src.common.babel import babel, get_locale
@@ -7,6 +8,7 @@ from src.common.babel import babel, get_locale
 
 def create_app(env: str = 'development') -> Flask:
     app = Flask(__name__)
+    jwt = JWTManager(app)
     app.config.from_object(f'src.common.configs.app.{env.capitalize()}Config')
     babel.init_app(app, locale_selector=get_locale)
     init_db(app)
