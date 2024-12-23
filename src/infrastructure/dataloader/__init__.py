9b84f7cad3375314c8195702d6f4d1eed1c5dbb0
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-12-23 15:24:17 +0500

    super admin jaratıw qosıldı, language benen bolǵan mashqala saplastırıldı

--- src/infrastructure/dataloader/__init__.py
+++ src/infrastructure/dataloader/__init__.py
@@ -3,11 +3,13 @@ from .role import load_roles
 from .permission import load_permissions
 from .associations import set_permissions_for_the_manager_role, set_permissions_for_the_super_admin_role
 from .language import load_language
+from .user import load_user
 
 def load_data(db):
     init_nukus_branch(db)
+    load_language(db)
     load_roles(db)
     load_permissions(db)
     set_permissions_for_the_super_admin_role(db)
     set_permissions_for_the_manager_role(db)
-    load_language(db)
+    load_user(db)
