3b936136d7c938060c0c1010f5f3aa4530fe0736
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-12-24 14:56:31 +0500

    file upload endpoint permission menen qorǵap qoyıldı

--- src/routes/admin/file.py
+++ src/routes/admin/file.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, send_from_directory
+from flask import Blueprint
 from src.infrastructure.controllers.file import FileController
+from src.middlewares import has_permission
 
 
 file_routes = Blueprint('admin-file', __name__)
 
 
 @file_routes.route('/upload-file', methods=['POST'])
+@has_permission('FILE_UPLOAD')
 def upload_file():
     return FileController.upload_file()
 
 @file_routes.route('/upload-files', methods=['POST'])
+@has_permission('FILE_UPLOAD')
 def upload_files():
     return FileController.upload_files()
 
